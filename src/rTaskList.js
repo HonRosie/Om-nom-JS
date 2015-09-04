e0ce4d1bd19bb38122451a04cdefc6a09505a479
HonRosie <gchou8@gmail.com>
2015-09-03 18:00:29 -0700

    Refactor Om nom to have consolidated app data and event dispatch

--- src/rTaskList.js
+++ src/rTaskList.js
@@ -1,95 +1,31 @@
 var node = document.getElementById("taskList");
 
-function toggleDone(task){
-  task.doneness = !task.doneness;
-}
+appData = {
+  projRootId: "root",
+  viewDone: false,
+  taskId: 0,
+  projectList: [],
+  focusTaskId: 0,
+  taskList: {"root": {id: "root", subtasks: [0]},
+                0: {id: 0,
+                    desc: "",
+                    parentId: "root",
+                    doneness: false,
+                    subtasks: [],
+                    isProjHeader: false,
+                    duedate: "",
+                    isSelected: false}}
+};
 
-function save(task, args){
-  task.desc = args.value;
-}
-
-function addTask(task){
-  var newId = ++taskId;
-  taskList[newId] = {id: newId, desc: "", parentId: task.parentId,
-                     doneness: false, subtasks: [], isProjHeader: false};
-  var parent = taskList[task.parentId];
-  var ix = parent.subtasks.indexOf(task.id) + 1;
-  parent.subtasks.splice(ix, 0, newId);
-  focusTaskId = newId;
-}
-
-function unsubTask(task){
-  var parent = taskList[task.parentId];
-  if (parent.id != "root"){
-    var parentParentId = parent.parentId;
-    var parentParent = taskList[parentParentId];
-
-    var ixParent = parentParent.subtasks.indexOf(parent.id);
-    parentParent.subtasks.splice(ixParent+1, 0, task.id);
-    task.parentId = parentParentId;
-
-    var ixTask = parent.subtasks.indexOf(task.id);
-    parent.subtasks.splice(ixTask, 1);
-    focusTaskId = task.id;
-  }
-}
-
-function subTask(task){
-  var parent = taskList[task.parentId];
-
-  var ixTask = parent.subtasks.indexOf(task.id)
-  if (ixTask !== 0) {
-    var newParentId = parent.subtasks[ixTask-1]
-    var newParent = taskList[newParentId]
-    newParent.subtasks.push(task.id)
-
-    task.parentId = newParentId
-    parent.subtasks.splice(ixTask, 1)
-  }
-  focusTaskId = task.id;
-}
-
-function deleteTask(task) {
-  var parentId = task.parentId;
-  var ix = taskList[parentId].subtasks.indexOf(task.id);
-  taskList[parentId].subtasks.splice(ix, 1);
-
-  delTask(task);
-
-}
-
-function delTask(task) {
-  var taskId = task.id;
-  var subTaskIds = task.subtasks
-  if (subTaskIds) {
-    subTaskIds.map(function(subTaskId){
-      delTask(taskList[subTaskId]);
-    })
-  }
-  delete taskList[taskId];
-}
-
-function toggleProject(task) {
-  task.isProjHeader = !task.isProjHeader;
-  if (task.isProjHeader) {
-    projectList.push(task.id);
-  }
-  else {
-    var ix = projectList.indexOf(task.id);
-    projectList.splice(ix, 1);
-  }
-}
-
-function setDate(task, args) {
-  task.duedate = args.value;
-}
-
-function assignDate(task) {
-  task.toAssignDate = !task.toAssignDate;
-}
-
+//---------------------------------------------------------
+// Dispatch
+// Handles all the events in the application. Every event
+// passes through the dispatcher which then calls the
+// appropriate functions
+//---------------------------------------------------------
 
 function eventDispatch(action, task, args){
+  var shouldDraw = true;
   switch(action){
     case "toggleDone":
       toggleDone(task);
@@ -109,22 +45,147 @@ function eventDispatch(action, task, args){
     case "delete":
       deleteTask(task);
       break;
-    case "toggleProject":
-      toggleProject(task);
+    case "isProjToggle":
+      isProjToggle(task);
       break;
     case "setDate":
       setDate(task, args);
       break;
-    case "assignDate":
-      assignDate(task);
+    case "toggleSelected":
+      toggleSelected(task);
+      break;
+    case "gotoProject":
+      gotoProject(task);
+      break;
+    case "massAssignDate":
+      massAssignDate(args)
       break;
   }
+
+  localStorage["appData"] = JSON.stringify(appData);
+  if(shouldDraw) {
+    draw();
+  }
 }
+
+function save(task, args){
+  task.desc = args.value;
+}
+
+function addTask(task){
+  var newId = ++appData.taskId;
+  appData.taskList[newId] = {id: newId, desc: "", parentId: task.parentId,
+                     doneness: false, subtasks: [], isProjHeader: false};
+
+  //get Parent
+  var parentTask = appData.taskList[task.parentId];
+  var ixOfCurrTask = parentTask.subtasks.indexOf(task.id) + 1;
+  //insert new task after current task
+  parentTask.subtasks.splice(ixOfCurrTask, 0, newId);
+  appData.focusTaskId = newId;
+}
+
+function deleteTask(task) {
+  //remove task from parent's subtask list
+  var parent = appData.taskList[task.parentId];
+  var parentSubtasks = parent.subtasks
+  var ix = parentSubtasks.indexOf(task.id);
+  parentSubtasks.splice(ix, 1);
+
+  //recursively delete task's subtasks
+  delSubtasks(task);
+}
+
+function delSubtasks(task) {
+  var taskId = task.id;
+  var subTaskIds = task.subtasks
+  if (subTaskIds) {
+    subTaskIds.map(function(subTaskId){
+      delSubtasks(appData.taskList[subTaskId]);
+    })
+  }
+  delete appData.taskList[taskId];
+}
+
+function unsubTask(task){
+  var parent = appData.taskList[task.parentId];
+  if (parent.id != "root"){
+    var parentParentId = parent.parentId;
+    var parentParent = appData.taskList[parentParentId];
+
+    //insert current task after it's previous parent under the now shared parent
+    var ixParent = parentParent.subtasks.indexOf(parent.id);
+    parentParent.subtasks.splice(ixParent+1, 0, task.id);
+    task.parentId = parentParentId;
+
+    //remove current task from it's previous parent subtask list
+    var ixTask = parent.subtasks.indexOf(task.id);
+    parent.subtasks.splice(ixTask, 1);
+    appData.focusTaskId = task.id;
+  }
+}
+
+function subTask(task){
+  var parent = appData.taskList[task.parentId];
+
+  var ixTask = parent.subtasks.indexOf(task.id)
+  //Make currtask subtask of whatever task is "before" it. If there is no task "before" it, don't subtask
+  if (ixTask !== 0) {
+    var newParentId = parent.subtasks[ixTask-1]
+    var newParent = appData.taskList[newParentId]
+    newParent.subtasks.push(task.id)
+
+    task.parentId = newParentId
+    parent.subtasks.splice(ixTask, 1)
+  }
+  appData.focusTaskId = task.id;
+}
+
+function toggleDone(task){
+  task.doneness = !task.doneness;
+}
+
+function isProjToggle(task) {
+  task.isProjHeader = !task.isProjHeader;
+  if (task.isProjHeader) {
+    appData.projectList.push(task.id);
+  }
+  else {
+    var ix = appData.projectList.indexOf(task.id);
+    appData.projectList.splice(ix, 1);
+  }
+}
+
+function setDate(task, args) {
+  task.duedate = args.value;
+}
+
+function toggleSelected(task) {
+  task.isSelected = !task.isSelected;
+}
+
+function gotoProject(task) {
+    appData.projRootId = appData.projRootId === "root" ? task.id : "root";
+}
+
+function massAssignDate(args) {
+  for (var taskId in appData.taskList) {
+    if (appData.taskList[taskId].isSelected == true) {
+      appData.taskList[taskId].duedate = args.value;
+      appData.taskList[taskId].isSelected = false;
+    }
+  }
+}
+
+
+
+//---------------------------------------------------------
+// Individual task component
+//---------------------------------------------------------
 
 var Task = React.createClass({
   save: function(e) {
     eventDispatch("save", this.props.task, {value: e.target.value});
-    draw();
   },
   handleKey: function(e) {
     //add task
@@ -151,84 +212,81 @@ var Task = React.createClass({
       eventDispatch("delete", this.props.task);
     }
     else if (e.shiftKey && e.keyCode === 186){
-      eventDispatch("toggleProject", this.props.task);
+      eventDispatch("isProjToggle", this.props.task);
     }
-    draw();
   },
-  handleButton: function() {
-    projRootId = projRootId === "root" ? this.props.task.id : "root";
-    draw();
+  gotoProject: function() {
+    eventDispatch("gotoProject", this.props.task);
+//     projRootId = projRootId === "root" ? this.props.task.id : "root";
   },
   handleDateKeys: function(e) {
     eventDispatch("setDate", this.props.task, {value: e.target.value});
-    draw();
   },
-  assignDate: function() {
-    eventDispatch("assignDate", this.props.task);
+  toggleSelected: function() {
+    eventDispatch("toggleSelected", this.props.task);
   },
   componentDidMount: function() {
-    if (this.props.task.id === focusTaskId) {
-      debugger;
+    if (this.props.task.id === appData.focusTaskId) {
       React.findDOMNode(this.refs.tasks).focus();
     }
   },
   render: function() {
     var task = this.props.task;
-    var gotoProj;
-    var duedate;
 
-    if (task.doneness && !viewDone) {
+    //If task is done and viewDone is not set, don't display
+    if (task.doneness && !appData.viewDone) {
       return null;
     }
-    else {
-      var cName = task.doneness ? "done " : "";
-      cName += task.isProjHeader ? "projHeader " : "";
 
-      if (task.isProjHeader) {
-        gotoProj = <button
-                      name="button"
-                      onClick={this.handleButton}>
-                   </button>
-      }
+    //Setting classname for css
+    var className = task.doneness ? "done " : "";
+    className += task.isProjHeader ? "projHeader " : "";
 
-
-      var subtasks = task.subtasks.map(function(taskId){
-        return <Task task={taskList[taskId]}
-                     key={taskId}
-                     viewDone={viewDone} />
-      })
-
-      return <li>
-                {gotoProj}
-                <input
-                  className="assignDate"
-                  type="checkbox"
-                  checked={task.toAssignDate}
-                  onChange={this.assignDate}
-                  >
-                </input>
-                <input
-                  className={cName}
-                  ref="tasks"
-                  onKeyDown={this.handleKey}
-                  onInput={this.save}
-                  value={task.desc}>
-                </input>
-                <input
-                  onInput={this.handleDateKeys}
-                  value={task.duedate}>
-                </input>
-                <ul>
-                  {subtasks}
-                </ul>
-             </li>
+    var gotoProj;
+    if (task.isProjHeader) {
+      gotoProj = <button
+                    name="button"
+                    onClick={this.gotoProject}>
+                 </button>
     }
+
+
+    var subtasks = task.subtasks.map(function(taskId){
+      return <Task task={appData.taskList[taskId]}
+                   key={taskId}
+                   viewDone={appData.viewDone} />
+    })
+
+    return <li>
+              {gotoProj}
+              <input
+                className="assignDate"
+                type="checkbox"
+                checked={task.isSelected}
+                onChange={this.toggleSelected}
+                >
+              </input>
+              <input
+                className={className}
+                ref="tasks"
+                onKeyDown={this.handleKey}
+                onInput={this.save}
+                value={task.desc}>
+              </input>
+              <input
+                onInput={this.handleDateKeys}
+                value={task.duedate}>
+              </input>
+              <ul>
+                {subtasks}
+              </ul>
+           </li>
   }
 });
 
 var TaskListFilters = React.createClass({
   handleUserInput: function() {
-    viewDone = this.refs.viewDoneCheck.getDOMNode().checked;
+    appData.viewDone = this.refs.viewDoneCheck.getDOMNode().checked;
     draw();
   },
   render: function() {
@@ -245,17 +303,17 @@ var TaskListFilters = React.createClass({
 
 var TaskList = React.createClass({
   render: function() {
-    if (projRootId === "root") {
-      var roots = this.props.tasks[projRootId].subtasks;
+    if (appData.projRootId === "root") {
+      var roots = this.props.tasks[appData.projRootId].subtasks;
       var tasks = roots.map(function(taskId){
-        return <Task task={taskList[taskId]}
+        return <Task task={appData.taskList[taskId]}
                      key={taskId}
-                     viewDone={viewDone} />
+                     viewDone={appData.viewDone} />
       })
     }
     else {
-      var tasks = <Task task={taskList[projRootId]}
-                        viewDone={viewDone} />
+      var tasks = <Task task={appData.taskList[appData.projRootId]}
+                        viewDone={appData.viewDone} />
     }
 
     return <ul>
@@ -278,14 +336,9 @@ var FilterTaskList = React.createClass({
 
 var AssignDate = React.createClass({
   assignDate: function(e) {
-    var roots = this.props.tasks["root"].subtasks;
-    roots.map(function(taskId){
-      if (taskList[taskId].toAssignDate == true) {
-        taskList[taskId].duedate = "foo"
-        taskList[taskId].toAssignDate = false
-      }
-      draw();
-    })
+    if (e.keyCode === 13) {
+      eventDispatch("massAssignDate", null, {value: e.currentTarget.value});
+    }
   },
   render: function() {
     return (
@@ -304,37 +357,24 @@ var ProjView = React.createClass({
       <div>
         <AssignDate tasks={this.props.tasks}/>
         <FilterTaskList tasks={this.props.tasks}
-                        viewDone={viewDone} />
+                        viewDone={appData.viewDone} />
       </div>
     )
   }
 });
 
-var projRootId = "root";
-var viewDone = false;
-// localStorage.clear();
 
-//Setting up task Ids and reloading from local storage
-var taskId = 0;
-if(localStorage["taskId"]) {
-  taskId = JSON.parse(localStorage["taskId"]);
+function init() {
+  if (localStorage["appData"]) {
+    appData = JSON.parse(localStorage["appData"]);
+  }
+  draw();
 }
 
-//Setting up tasklist structure and reloading it from local storage
-var projectList = [];
-var focusTaskId = 0;
-var taskList = {"root": {id: "root", subtasks: [0]},
-                0: {id: 0, desc: "", parentId: "root", doneness: false,
-                    subtasks: [], isProjHeader: false, duedate: "", toAssignDate: false}};
-
-if(localStorage["taskList"]) {
-  taskList = JSON.parse(localStorage["taskList"]);
-}
+init();
 
 function draw(){
-  localStorage["taskId"] = JSON.stringify(taskId);
-  localStorage["taskList"] = JSON.stringify(taskList);
-  React.render(<ProjView tasks={taskList} />, node);
+  React.render(<ProjView tasks={appData.taskList} />, node);
 }
 
-draw();
+// localStorage.clear();
